59df853bf0fe45792ec6c4accd5156bbd6b3048b
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-07-25 09:48:09 +0300

    add tasks

---
+++ js/module_5/study_tasks/task_01_class.js
@@ -0,0 +1,25 @@
+class Storage {
+  constructor(object) {
+    this.items = object;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    return this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    return this.items.splice(this.items.indexOf(itemToRemove), 1);
+  }
+}
+
+// Change code above this line
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
