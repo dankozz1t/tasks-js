48db4985d1ea66f16efbe84610418fd5b4f46069
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-07-28 20:45:56 +0300

    edit task regexp

--- js/other/codewars/5kyu/task_01_regexp.js
+++ js/other/codewars/5kyu/task_01_regexp.js
@@ -15,16 +15,18 @@
 //   return newStr.join(' ');
 // }
 
-pigIt = str =>
-  str
-    .split(' ')
-    .reduce((acc, word) => {
-      if (!word.includes('!')) {
-        return acc.concat([word.slice(1, word.length) + word[0] + 'ay']);
-      }
-      return acc;
-    }, [])
-    .join(' ');
+// pigIt = str =>
+//   str
+//     .split(' ')
+//     .reduce((acc, word) => {
+//       if (!word.includes('!')) {
+//         return acc.concat([word.slice(1, word.length) + word[0] + 'ay']);
+//       }
+//       return acc;
+//     }, [])
+//     .join(' ');
+
+pigIt = str => (str = str.replace(/(\w)(\w*)/g, '$2$1ay'));
 
 console.log(pigIt('Pig latin is cool')); //'igPay atinlay siay oolcay'
 console.log(pigIt('Oay emporatay oay oresmay !ay')); //'Oay emporatay oay oresmay !'
